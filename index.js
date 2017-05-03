e407cad3f179ad2542b057890eabaedfc9257839
Florian Schroedl <hello@florianschroedl.com>
2017-05-03 14:00:46 +0200

    Loop over rc props

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import * as utils from './lib/utils'
+import expandHomeDir from 'expand-home-dir'
 
 const rc = '~/.homesick/repos/dotfiles/settings/syncsettings.yaml'
 const settings = utils.loadSettingsFileSync(rc)
 
-console.log(settings)
+for (let prop in settings) {
+  if (settings.hasOwnProperty(prop)) {
+    console.log(prop)
+  }
+}
