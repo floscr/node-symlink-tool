62541059b8129003c93adbff3003103f1d525cea
Florian Schroedl <hello@florianschroedl.com>
2017-05-04 19:13:36 +0200

    Map all settings to flat array

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import * as utils from './lib/utils'
 import expandHomeDir from 'expand-home-dir'
 
+const { log } = utils
+
 const rc = '~/.homesick/repos/dotfiles/settings/syncsettings.yaml'
 const settings = utils.loadSettingsFileSync(rc)
 
+let files = []
 for (let prop in settings) {
-  if (settings.hasOwnProperty(prop)) {
-    console.log(prop)
+  if (settings.hasOwnProperty(prop) && settings[prop]) {
+    files.push(
+      ...settings[prop].map(({ src, dst }) => ({ src, dst, type: prop }))
+    )
   }
 }
+
+log(files)
+
+// | parentDirectoryExists(dst)
+// | fileExists(src)
+// | link(src, dst)
+
