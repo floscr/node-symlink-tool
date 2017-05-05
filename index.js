7d6a34250f3cc35a36d354c0741f20735ea2e9d3
Florian Schroedl <hello@florianschroedl.com>
2017-05-05 11:51:49 +0200

    Add logging system

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import { log, logErr } from './lib/utils'
 import * as fsUtil from './lib/fileSystem.js'
 import _ from 'lodash'
 
-const settingsFile = '~/.homesick/repos/dotfiles/settings/syncsettings.yaml'
+function main (dryRun) {
+  const settingsFile = '~/.homesick/repos/dotfiles/settings/syncsettings.yaml'
 
-const settings = fsUtil.loadSettings(settingsFile)
+  const settings = fsUtil.loadSettings(settingsFile)
 
-const promises = settings.map(file => fsUtil.createLink(file))
+  const promises = settings
+    .map(file => fsUtil.createLink(file, dryRun))
 
-Promise.all(promises)
-  .then(() => { console.log('Done!') })
-  .catch(console.error)
+  Promise.all(promises)
+    .then(() => { console.log('Done!') })
+    .catch(console.error)
+}
+
+main(true)
