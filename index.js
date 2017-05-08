964350f46e62136b4e4a13ee2f10b2e3199f6206
Florian Schroedl <hello@florianschroedl.com>
2017-05-08 12:12:33 +0200

    Rename defaultOptions to defaultArgs

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-import * as fsUtil from './lib/fileSystem.js'
-import expandHomeDir from 'expand-home-dir'
-import defaultOptions from './lib/defaultOptions'
 import R from 'ramda'
 import path from 'path'
+import expandHomeDir from 'expand-home-dir'
 
-export default function main (customOptions) {
-  const options = R.merge(customOptions, defaultOptions)
-  const baseDir = expandHomeDir(options.baseDir)
+import * as fsUtil from './lib/fileSystem.js'
+import { defaultArgs } from './lib/defaultArgs'
+
+export default function main (customArgs) {
+  const args = R.merge(customArgs, defaultArgs)
+  const baseDir = expandHomeDir(args.baseDir)
   const settingsFile = path.join(baseDir, 'syncsettings.yaml')
 
   const settings = fsUtil.loadSettings(settingsFile)
 
   const promises = settings
-    .map(file => fsUtil.createLink(file, options))
+    .map(file => fsUtil.createLink(file, args))
 
   Promise.all(promises)
     .then(() => { console.log('Done!') })
