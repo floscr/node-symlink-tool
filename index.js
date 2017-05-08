3e8aeac7944ca7cbb3732d6030d3d0b96e79edc8
Florian Schroedl <hello@florianschroedl.com>
2017-05-08 12:37:12 +0200

    Rename fsutil import to lib

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import R from 'ramda'
 import path from 'path'
 import expandHomeDir from 'expand-home-dir'
 
-import * as fsUtil from './lib/fileSystem.js'
+import * as lib from './lib/fileSystem.js'
 import { defaultArgs } from './lib/defaultOptions'
 
 export default function main (customArgs) {
   const args = R.merge(defaultArgs, customArgs)
 
-  const settings = fsUtil.loadSettings(args)
+  const settings = lib.loadSettings(args)
 
   const promises = settings
-    .map(file => fsUtil.createLink(args, file))
+    .map(file => lib.createLink(args, file))
 
   Promise.all(promises)
     .then(() => { console.log('Done!') })
